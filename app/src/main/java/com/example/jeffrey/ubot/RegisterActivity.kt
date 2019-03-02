4990d2b0c3502db8264c813a2f3ff83a91549833
jsun454 <jsun46@umail.ucsb.edu>
2019-03-02 02:48:46 -0800

    Linked project with Firebase

--- app/src/main/java/com/example/jeffrey/ubot/RegisterActivity.kt
+++ app/src/main/java/com/example/jeffrey/ubot/RegisterActivity.kt
@@ -2,11 +2,19 @@ package com.example.jeffrey.ubot
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = RegisterActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "Hello")
     }
 }
