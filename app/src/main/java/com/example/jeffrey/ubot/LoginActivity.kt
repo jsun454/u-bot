9594a9f19bbc8d464052e97626a798d6e042c071
jsun454 <jsun46@umail.ucsb.edu>
2019-03-02 15:24:53 -0800

    Implemented switching between login and register activities

--- app/src/main/java/com/example/jeffrey/ubot/LoginActivity.kt
+++ app/src/main/java/com/example/jeffrey/ubot/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.example.jeffrey.ubot
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = LoginActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Log.i(TAG, TAG)
+
+        activity_login_txt_go_register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/jeffrey/ubot/RegisterActivity.kt
+++ app/src/main/java/com/example/jeffrey/ubot/RegisterActivity.kt
@@ -1,8 +1,10 @@
 package com.example.jeffrey.ubot
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -14,6 +16,13 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        Log.i(TAG, "Hello")
+        Log.i(TAG, TAG)
+
+        activity_register_txt_go_login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
