277f7db75535fe93825a019dc08e50bc1a642127
jsun454 <jsun46@umail.ucsb.edu>
2019-03-02 18:15:20 -0800

    Implemented basic chat room layout, chat room opens from bot list

--- app/src/main/java/com/example/jeffrey/ubot/BotListActivity.kt
+++ app/src/main/java/com/example/jeffrey/ubot/BotListActivity.kt
@@ -1,12 +1,38 @@
 package com.example.jeffrey.ubot
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_bot_list.*
 
 class BotListActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = BotListActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bot_list)
+
+        Log.i(TAG, "Current activity: $TAG")
+
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        activity_bot_list_btn_u_bot.setOnClickListener {
+            showActivity(ChatRoomActivity::class.java)
+        }
+
+        activity_bot_list_btn_community_bot.setOnClickListener {
+
+        }
+    }
+
+    private fun <T: Any> showActivity(activity: Class<T>) {
+        val intent = Intent(this, activity)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/jeffrey/ubot/ChatRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jeffrey.ubot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatRoomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_room)
+    }
+}
