6aa7a3192e268cf64ed60b9edc494297194179ed
jsun454 <jsun46@umail.ucsb.edu>
2019-03-02 16:15:13 -0800

    Implemented Firebase Authentication user registration and login

--- app/src/main/java/com/example/jeffrey/ubot/LoginActivity.kt
+++ app/src/main/java/com/example/jeffrey/ubot/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -16,13 +18,49 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        Log.i(TAG, TAG)
+        Log.i(TAG, "Current activity: $TAG")
+
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        activity_login_btn_login.setOnClickListener {
+            loginUser()
+        }
 
         activity_login_txt_go_register.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-            finish()
+            showRegisterActivity()
         }
+    }
+
+    private fun loginUser() {
+        val email = activity_login_et_email.text.toString()
+        val password = activity_login_et_password.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        activity_login_btn_login.isClickable = false
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                Log.i(TAG, "Successfully logged in as user: ${it.user.uid}")
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "Failed to log in as user: ${it.message}")
+                Toast.makeText(this, "Login failed: ${it.message}", Toast.LENGTH_SHORT).show()
+
+                activity_login_btn_login.isClickable = true
+            }
+    }
+
+    private fun showRegisterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+
+        finish()
     }
 }
--- app/src/main/java/com/example/jeffrey/ubot/RegisterActivity.kt
+++ app/src/main/java/com/example/jeffrey/ubot/RegisterActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -16,13 +18,51 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        Log.i(TAG, TAG)
+        Log.i(TAG, "Current activity: $TAG")
+
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        activity_register_btn_register.setOnClickListener {
+            registerUser()
+        }
 
         activity_register_txt_go_login.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-            finish()
+            showLoginActivity()
         }
+    }
+
+    private fun registerUser() {
+        val email = activity_register_et_email.text.toString()
+        val password = activity_register_et_password.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        activity_register_btn_register.isClickable = false
+
+        Toast.makeText(this, "Creating user...", Toast.LENGTH_SHORT).show()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                Log.i(TAG, "Successfully created user: ${it.user.uid}")
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Registration failed: ${it.message}", Toast.LENGTH_SHORT).show()
+
+                activity_register_btn_register.isClickable = true
+            }
+    }
+
+    private fun showLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+
+        finish()
     }
 }
