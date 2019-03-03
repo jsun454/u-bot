64ae99394bfdb0522e07e9f61e4a45b2eaacb54c
jsun454 <jsun46@umail.ucsb.edu>
2019-03-02 22:59:57 -0800

    User messages are displayed in recycler view, user can send new messages

--- app/src/main/java/com/example/jeffrey/ubot/ChatRoomActivity.kt
+++ app/src/main/java/com/example/jeffrey/ubot/ChatRoomActivity.kt
@@ -5,8 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import com.example.jeffrey.ubot.model.Bot
 import com.example.jeffrey.ubot.model.Message
+import com.example.jeffrey.ubot.view.BotMessageItem
+import com.example.jeffrey.ubot.view.UserMessageItem
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_chat_room.*
 
 class ChatRoomActivity : AppCompatActivity() {
@@ -15,16 +22,51 @@ class ChatRoomActivity : AppCompatActivity() {
         private val TAG = ChatRoomActivity::class.java.simpleName
     }
 
+    private val adapter = GroupAdapter<ViewHolder>()
+    private var bot: Bot? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
 
         Log.i(TAG, "Current activity: $TAG")
 
-        setOnClickListeners()
+        setRoomName()
+        setMessageListener()
+        setOnClickListener()
     }
 
-    private fun setOnClickListeners() {
+    private fun setRoomName() {
+        bot = intent.getParcelableExtra(BotListActivity.ROOM_KEY)
+        supportActionBar?.title = bot?.name ?: "Bot"
+    }
+
+    private fun setMessageListener() {
+        activity_chat_room_rv_message_list.adapter = adapter
+
+        val user = FirebaseAuth.getInstance().uid ?: return
+        val botFile = bot?.fileName ?: return
+
+        val ref = FirebaseDatabase.getInstance().getReference("/personal-messages/$user/$botFile")
+
+        ref.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                val message = p0.getValue(Message::class.java) ?: return
+                if(message.senderId == user) {
+                    adapter.add(UserMessageItem(message.message))
+                } else {
+                    adapter.add(BotMessageItem(message.message))
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {}
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {}
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {}
+            override fun onChildRemoved(p0: DataSnapshot) {}
+        })
+    }
+
+    private fun setOnClickListener() {
         activity_chat_room_btn_send.setOnClickListener {
             sendMessage()
         }
@@ -38,9 +80,7 @@ class ChatRoomActivity : AppCompatActivity() {
         }
 
         val user = FirebaseAuth.getInstance().uid ?: return
-
-        val bot = intent.getParcelableExtra<Bot>(BotListActivity.ROOM_KEY)
-        val botFile = bot.fileName
+        val botFile = bot?.fileName ?: return
 
         val userRef = FirebaseDatabase.getInstance().getReference("/personal-messages/$user/$botFile").push()
         val communityRef = FirebaseDatabase.getInstance().getReference("/community-messages/").push()
@@ -50,6 +90,7 @@ class ChatRoomActivity : AppCompatActivity() {
         userRef.setValue(message)
             .addOnSuccessListener {
                 Log.i(TAG, "Successfully saved message")
+                activity_chat_room_et_user_message.text.clear()
             }
             .addOnFailureListener {
                 Log.e(TAG, "Failed to save message: ${it.message}")
