f23edc165381b91d690102dfd045ced81e47c61a
jsun454 <jsun46@umail.ucsb.edu>
2019-03-02 22:59:04 -0800

    Added default constructor for messages

--- app/src/main/java/com/example/jeffrey/ubot/model/Message.kt
+++ app/src/main/java/com/example/jeffrey/ubot/model/Message.kt
@@ -1,3 +1,5 @@
 package com.example.jeffrey.ubot.model
 
-class Message(val id: String, val senderId: String?, val message: String, val timeStamp: Long)
+class Message(val id: String, val senderId: String?, val message: String, val timeStamp: Long) {
+    constructor(): this("", "", "", -1)
+}
